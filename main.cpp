75732edbedaf5625f644a6bfb25a63d927c643d0
Andreev.Alexander <dragonskull2009@gmail.com>
2026-04-04 16:11:36 +0300

    стартовый коммит

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+int main() {
+
+
+
+
+    while (true) {
+
+        unsigned short int choice = menu();
+        switch (choice) {
+        case 0:
+            return 0;
+        case 1:
+            return "case 1";
+        case 9:
+            break;
+
+
+
+        }
+
+
+    }
+}
